c23ddbfff71626c9c6e951e9545e863d10fcdae1
Maik96 <maikorenner96@gmail.com>
2023-08-20 16:05:25 +0200

    Added  images and implemented them into Welcome Screen

--- lib/widgets/welcomescreen/previews_movies.dart
+++ lib/widgets/welcomescreen/previews_movies.dart
@@ -15,41 +15,69 @@ class PreviewMovies extends StatelessWidget {
           decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(30),
               border: Border.all(
-                color: Colors.white,
+                color: const Color.fromARGB(255, 0, 0, 0),
                 width: 2,
               )),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(30),
+            child: Image.asset(
+              "assets/images/ConjuringPicture.jpg",
+              fit: BoxFit.fill,
+            ),
+          ),
         ),
         Container(
-            height: 290,
-            width: 190,
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(30),
-                border: Border.all(
-                  color: Colors.white,
-                  width: 2,
-                )))
+          height: 290,
+          width: 190,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(30),
+              border: Border.all(
+                color: const Color.fromARGB(255, 0, 0, 0),
+                width: 2,
+              )),
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(30),
+            child: Image.asset("assets/images/InterstellarPicture.jpg",
+                fit: BoxFit.fill),
+          ),
+        )
       ]),
       Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           Container(
-              height: 290,
-              width: 190,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(30),
-                  border: Border.all(
-                    color: Colors.white,
-                    width: 2,
-                  ))),
+            height: 290,
+            width: 190,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                border: Border.all(
+                  color: const Color.fromARGB(255, 0, 0, 0),
+                  width: 2,
+                )),
+            child: ClipRRect(
+                borderRadius: BorderRadius.circular(30),
+                child: Image.asset(
+                  "assets/images/StarWarsPicture.jpg",
+                  fit: BoxFit.fill,
+                )),
+          ),
           Container(
-              height: 290,
-              width: 190,
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(30),
-                  border: Border.all(
-                    color: Colors.white,
-                    width: 2,
-                  ))),
+            height: 290,
+            width: 190,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(30),
+                border: Border.all(
+                  color: const Color.fromARGB(255, 0, 0, 0),
+                  width: 2,
+                )),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(30),
+              child: Image.asset(
+                "assets/images/WolfOfWallStreetPicture.jpg",
+                fit: BoxFit.fill,
+              ),
+            ),
+          ),
         ],
       ),
     ]);
