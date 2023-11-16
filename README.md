bf0770284978793f10b1d2788666e80ae0dedfb6
Maik96 <maikorenner96@gmail.com>
2023-11-16 15:24:37 +0100

    Added  image to read.me

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ A Movie app to find and save all our favorites movies.
 
 Still in progress.. 
 
+(assets/images/image1.jpeg)
+
+
+
--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -22,9 +22,26 @@ class _HomeScreenState extends State<HomeScreen> {
         });
       } else if (Platform.isIOS) {
         return Container(
-          height: 200,
-          width: 200,
-        );
+            alignment: Alignment.center,
+            color: const Color.fromARGB(255, 0, 0, 0),
+            child: Container(
+              height: 100.h,
+              width: 100.w,
+              decoration: BoxDecoration(
+                  border: Border.all(color: Colors.white, width: 2)),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  Container(
+                    margin: EdgeInsets.only(top: 5.h),
+                    height: 30,
+                    width: 70,
+                    color: Colors.white,
+                  ),
+                ],
+              ),
+            ));
       }
 
       return ErrorWidget("Error");
