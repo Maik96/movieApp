bd8ab1ba11435adedc21af5edc74a9eceb9a4b46
Maik96 <maikorenner96@gmail.com>
2023-11-21 12:22:50 +0100

    Adding basics to the AddScreen

--- lib/screens/add_screen.dart
+++ lib/screens/add_screen.dart
@@ -1,5 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/src/widgets/framework.dart';
-import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter/widgets.dart';
 
 class AddScreen extends StatefulWidget {
   const AddScreen({super.key});
@@ -11,6 +13,15 @@ class AddScreen extends StatefulWidget {
 class _AddScreenState extends State<AddScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+        child: LayoutBuilder(builder: (BuildContext, BoxConstraints) {
+      if (Platform.isAndroid) {
+        return Container();
+      } else if (Platform.isIOS) {
+        return Container();
+      }
+
+      return ErrorWidget("Error");
+    }));
   }
 }
